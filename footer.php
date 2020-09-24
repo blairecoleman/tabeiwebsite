2e9261b088ec41040af1095bc3e841703c54f246
bacolema <bacolema@iupui.edu>
2020-09-24 00:33:11 -0400

    Merge branch 'master' of https://github.com/blairecoleman/tabeiwebsite process clickable map # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

---
+++ footer.php
@@ -0,0 +1,21 @@
+<?php
+print <<< HERE
+<footer>
+    <div class = "socialsgridfeat">
+        <!--for a grid of social media icons-->
+        <div class="grid-container">
+            <div class="socgrid-item">
+                <a href = ""><i class="fab fa-instagram"></i></a>
+            </div>
+            <div class="socgrid-item">
+                <a href = ""><i class="fab fa-linkedin"></i></a>
+            </div>
+            <div class="socgrid-item">
+                <a href = ""><i class="fas fa-laptop-code"></i></a>
+            </div>
+        </div>
+    </div>
+    <p>Proudly designed and developed by <span>Tabei</span> Design</p>
+    <small> &copy Tabei Design 2020</small>
+</footer>
+HERE;
